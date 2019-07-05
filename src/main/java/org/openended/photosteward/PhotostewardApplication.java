58f840805b89a5fa4e447742504a93ed6932caae
Jens Fischer <jfischer@iMac2011.local>
2019-07-05 21:33:58 +0200

    initial commit

---
+++ src/main/java/org/openended/photosteward/PhotostewardApplication.java
@@ -0,0 +1,13 @@
+package org.openended.photosteward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotostewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotostewardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/openended/photosteward/PhotostewardApplicationTests.java
@@ -0,0 +1,16 @@
+package org.openended.photosteward;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotostewardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
